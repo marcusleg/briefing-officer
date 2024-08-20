46d368133fb0248afd3359a71f20ae5bb7952434
Marcus Legendre <marcus.legendre@codecentric.de>
2024-08-20 17:01:50 +0200

    Show mark as read/unread button on AI Summary page

--- src/app/feed/[feedId]/[articleId]/ai-summary/page.tsx
+++ src/app/feed/[feedId]/[articleId]/ai-summary/page.tsx
@@ -3,12 +3,14 @@ import BackButton from "@/components/BackButton";
 import { Button } from "@/components/ui/button";
 import { Speech } from "lucide-react";
 import { getAiSummary } from "./actions";
+import ToggleReadButton from "@/components/ToggleReadButton";
 
 const AiSummary = async ({
   params,
 }: {
   params: { feedId: string; articleId: string };
 }) => {
+  let feedId = parseInt(params.feedId);
   let articleId = parseInt(params.articleId);
 
   const article = await prisma.article.findUniqueOrThrow({
@@ -23,6 +25,11 @@ const AiSummary = async ({
     <div className="m-2 max-w-4xl flex flex-col gap-2">
       <div className="flex flex-row gap-2">
         <BackButton />
+        <ToggleReadButton
+          feedId={feedId}
+          articleId={articleId}
+          isRead={article.read}
+        />
         <Button variant="outline">
           <Speech className="mr-2 h-4 w-4" />
           Read aloud
