21222b32293ef263ac025a5fa461877ae7857993
Marcus Legendre <marcus.legendre@codecentric.de>
2024-08-20 09:37:26 +0200

    Refresh page when refreshing feed

--- src/components/RefreshFeedButton.tsx
+++ src/components/RefreshFeedButton.tsx
@@ -1,12 +1,20 @@
 "use client";
 import { refreshFeed } from "@/app/actions";
+import { useRouter } from "next/navigation";
 
 interface RefreshFeedButtonProps {
   feedId: number;
 }
 
 const RefreshFeedButton = ({ feedId }: RefreshFeedButtonProps) => {
-  return <button onClick={() => refreshFeed(feedId)}>Refresh now</button>;
+  const router = useRouter();
+
+  const handleClick = async () => {
+    await refreshFeed(feedId);
+    router.refresh();
+  };
+
+  return <button onClick={handleClick}>Refresh now</button>;
 };
 
 export default RefreshFeedButton;
