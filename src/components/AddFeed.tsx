7934ea9f31739acb0d78d3f162e4341723483a64
Marcus Legendre <marcus.legendre@codecentric.de>
2024-08-21 11:55:44 +0200

    Use `Dialog` in `AddFeed` component

--- src/components/AddFeed.tsx
+++ src/components/AddFeed.tsx
@@ -3,7 +3,19 @@
 import { useState } from "react";
 import { addFeed } from "@/app/actions";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Plus } from "lucide-react";
 
 const AddFeed = () => {
   const [inputFeedUrl, setInputFeedUrl] = useState("");
@@ -11,19 +23,44 @@ const AddFeed = () => {
   const handleAddFeed = () => addFeed(inputFeedUrl);
 
   return (
-    <>
-      <h2 className="scroll-m-20 text-xl font-semibold tracking-tight">
-        Add feed
-      </h2>
-      <Input
-        type="url"
-        placeholder="Enter URL"
-        onChange={(event) => setInputFeedUrl(event.target.value)}
-      />
-      <Button variant="secondary" className="w-full" onClick={handleAddFeed}>
-        Add
-      </Button>
-    </>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="w-full" size="sm" variant="ghost">
+          <Plus className="mr-2 h-4 w-4" />
+          Add feed
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add a new feed</DialogTitle>
+          <DialogDescription>
+            Enter a URL of an RSS or Atom feed.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="flex items-center gap-4">
+            <Label htmlFor="url">URL</Label>
+            <Input
+              defaultValue={inputFeedUrl}
+              id="url"
+              onChange={(event) => setInputFeedUrl(event.target.value)}
+              placeholder="http://example.com/rss"
+              type="url"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button type="button" onClick={handleAddFeed}>
+            Add
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
