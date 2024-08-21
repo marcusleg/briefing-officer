bbadc064649c9a31e64bc579e5570ca6e7e69c49
Marcus Legendre <marcus.legendre@codecentric.de>
2024-08-21 10:47:54 +0200

    Add dropdown menu stub to `AdditionalFeedActionsButton`

--- src/components/feed/AdditionalFeedActionsButton.tsx
+++ src/components/feed/AdditionalFeedActionsButton.tsx
@@ -1,5 +1,14 @@
 import { Button } from "@/components/ui/button";
-import { Ellipsis } from "lucide-react";
+import { Ellipsis, Filter, Pencil, Trash } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 interface AdditionalFeedActionsButtonProps {
   feedId: number;
@@ -9,9 +18,27 @@ const AdditionalFeedActionsButton = ({
   feedId,
 }: AdditionalFeedActionsButtonProps) => {
   return (
-    <Button variant="outline" size="icon">
-      <Ellipsis />
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Ellipsis />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="start">
+        <DropdownMenuLabel>Feed</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Pencil className="mr-2 h-4 w-4" />
+            <span>Rename</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Trash className="mr-2 h-4 w-4" />
+            <span>Delete</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
