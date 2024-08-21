e077dd7184a327e4b078617d8c5ca69938ab42f8
Marcus Legendre <marcus.legendre@codecentric.de>
2024-08-21 09:56:13 +0200

    Disable Refresh button while refresh is in progress

--- src/components/feed/RefreshFeedButton.tsx
+++ src/components/feed/RefreshFeedButton.tsx
@@ -2,14 +2,27 @@
 import { refreshFeed } from "@/app/actions";
 import { Button } from "@/components/ui/button";
 import { RotateCw } from "lucide-react";
+import { useState } from "react";
 
 interface RefreshFeedButtonProps {
   feedId: number;
 }
 
 const RefreshFeedButton = ({ feedId }: RefreshFeedButtonProps) => {
+  const [refreshInProgress, setRefreshInProgress] = useState(false);
+
+  const handleCLick = async () => {
+    setRefreshInProgress(true);
+    await refreshFeed(feedId);
+    setRefreshInProgress(false);
+  };
+
   return (
-    <Button variant="outline" onClick={() => refreshFeed(feedId)}>
+    <Button
+      disabled={refreshInProgress}
+      onClick={handleCLick}
+      variant="outline"
+    >
       <RotateCw className="mr-2 h-4 w-4" />
       Refresh
     </Button>
