ad2c55864e99f53a763841638de46b5c8a766fce
Marcus Legendre <marcus.legendre@codecentric.de>
2024-08-22 20:00:05 +0200

    Remove frontpage code that was left over from early prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,4 @@
-import { parseFeed } from "htmlparser2";
-
-const feeds = [
-  "https://feeds.feedburner.com/Phoronix",
-  "https://www.heise.de/developer/rss/news-atom.xml",
-];
-
-const fetchFeeds = async () => {
-  const promises = feeds.map((feed) => fetch(feed).then((res) => res.text()));
-  const results = await Promise.all(promises);
-  return results.map((result) => parseFeed(result));
-};
-
 const Home = async () => {
-  const feeds = await fetchFeeds();
-
   return <main>This is the frontpage.</main>;
 };
 
