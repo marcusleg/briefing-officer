c0c61f1806e9f81433939c6d7ec2159456eb08c0
Marcus Legendre <marcus.legendre@codecentric.de>
2024-08-22 20:55:55 +0200

    feat: first attempt at making the frontpage like a dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
+import UnreadArticlesInTotal from "@/components/frontpage/UnreadArticlesInTotal";
+import UnreadArticlesPerFeedChart from "@/components/frontpage/UnreadArticlesPerFeedChart";
+import prisma from "@/lib/prismaClient";
+
 const Home = async () => {
-  return <main>This is the frontpage.</main>;
+  const feedWithUnreadArticleCount = await prisma.feed.findMany({
+    include: {
+      _count: {
+        select: {
+          articles: { where: { read: false } },
+        },
+      },
+    },
+  });
+  const chartData = feedWithUnreadArticleCount.map((feed) => {
+    return {
+      feedTitle: feed.title,
+      unread: feed._count.articles,
+    };
+  });
+
+  const unreadArticlesInTotal = feedWithUnreadArticleCount.reduce(
+    (acc, feed) => acc + feed._count.articles,
+    0,
+  );
+
+  return (
+    <main className="flex gap-4">
+      <UnreadArticlesInTotal unreadArticles={unreadArticlesInTotal} />
+      <UnreadArticlesPerFeedChart chartData={chartData} />
+    </main>
+  );
 };
 
 export default Home;
