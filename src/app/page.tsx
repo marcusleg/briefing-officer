906e8983986b65ab4fa2524afa12e43e1393bd65
Marcus Legendre <marcus.legendre@codecentric.de>
2024-08-23 23:34:11 +0200

    fix: use correct field in chart displaying articles per day

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ const Home = async () => {
           _all: true,
         },
         where: {
-          createdAt: {
+          publicationDate: {
             gte: new Date(dateRange.from),
             lt: new Date(dateRange.to),
           },
