067f494fd6114d49d7ec341e3f8c7cd9891fbd93
Marcus Legendre <marcus.legendre@codecentric.de>
2024-08-29 19:22:37 +0200

    fix: mark `VisitButton` as client component

--- src/components/article/VisitButton.tsx
+++ src/components/article/VisitButton.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { markArticleAsRead } from "@/app/feed/[feedId]/actions";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Article } from "@prisma/client";
