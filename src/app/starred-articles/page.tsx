bad6a61b326881b32cc889af33891e9df8ed2acc
Marcus Legendre <marcus.legendre@codecentric.de>
2024-09-03 17:39:48 +0200

    feat: Starred Articles page

---
+++ src/app/starred-articles/page.tsx
@@ -0,0 +1,42 @@
+import ArticleList from "@/components/article/ArticleList";
+import Typography from "@/components/ui/typography";
+import prisma from "@/lib/prismaClient";
+
+const getStarredArticles = () => {
+  return prisma.article.findMany({
+    include: {
+      aiSummary: true,
+      feed: true,
+      readability: true,
+    },
+    where: {
+      starred: true,
+    },
+    orderBy: {
+      publicationDate: "desc",
+    },
+  });
+};
+
+const StarredArticlesPage = async () => {
+  const articles = await getStarredArticles();
+
+  return (
+    <div className="flex flex-col gap-4">
+      <Typography variant="h2">Read Later</Typography>
+
+      {articles.length > 0 ? (
+        <ArticleList articles={articles} />
+      ) : (
+        <Typography
+          variant="p"
+          className="my-8 text-center text-lg text-muted-foreground"
+        >
+          You haven&apos;t starred any articles yet.
+        </Typography>
+      )}
+    </div>
+  );
+};
+
+export default StarredArticlesPage;
