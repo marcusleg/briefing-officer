2fc599df97c10b0aec12dd43a0ba9895f0cebc08
Marcus Legendre <marcus.legendre@codecentric.de>
2024-09-03 17:31:09 +0200

    feat: Read History page

---
+++ src/app/read-history/page.tsx
@@ -0,0 +1,36 @@
+import ArticleList from "@/components/article/ArticleList";
+import Typography from "@/components/ui/typography";
+import prisma from "@/lib/prismaClient";
+
+const getReadHistory = () => {
+  return prisma.article.findMany({
+    include: {
+      aiSummary: true,
+      feed: true,
+      readability: true,
+    },
+    where: {
+      readAt: {
+        not: null,
+      },
+    },
+    orderBy: {
+      readAt: "desc",
+    },
+    take: 50,
+  });
+};
+
+const ReadHistoryPage = async () => {
+  const articles = await getReadHistory();
+
+  return (
+    <div className="flex flex-col gap-4">
+      <Typography variant="h2">Read History</Typography>
+
+      <ArticleList articles={articles} />
+    </div>
+  );
+};
+
+export default ReadHistoryPage;
