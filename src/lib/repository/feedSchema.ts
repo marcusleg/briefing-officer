717457dbef9900f797444473b59747f927ec0d08
Marcus Legendre <marcus.legendre@codecentric.de>
2024-09-12 16:58:50 +0200

    feat: validate that title filter expression are valid regex

--- src/lib/repository/feedSchema.ts
+++ src/lib/repository/feedSchema.ts
@@ -3,6 +3,24 @@ import { z } from "zod";
 export const feedSchema = z.object({
   title: z.string(),
   link: z.string().url(),
-  titleFilterExpressions: z.string(),
+  titleFilterExpressions: z
+    .string()
+    // .transform((line) => line.split("\n"))
+    .refine(
+      (lines) => {
+        return lines.split("\n").every((line) => {
+          try {
+            new RegExp(line);
+            return true;
+          } catch (e) {
+            return false;
+          }
+        });
+      },
+      {
+        message:
+          "All title filter expressions must be valid regular expressions.",
+      },
+    ),
 });
 export type FeedSchema = z.infer<typeof feedSchema>;
