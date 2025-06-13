48e8ecbbcf9cca7f4389077a7a8d4ecbcb6cf9a1
Marcus Legendre <marcus.legendre@gmail.com>
2025-06-13 22:55:30 +0200

    refactor: replace inline feed links with `LeftNavigationLink` component

--- src/components/layout/FeedNavigation.tsx
+++ src/components/layout/FeedNavigation.tsx
@@ -1,11 +1,7 @@
-"use client";
+import LeftNavigationLink from "@/components/layout/LeftNavigationLink";
 import { Badge } from "@/components/ui/badge";
-import { buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 import { Prisma } from "@prisma/client";
 import { NewspaperIcon } from "lucide-react";
-import Link from "next/link";
-import { useParams } from "next/navigation";
 
 interface FeedNavigationProps {}
 
@@ -14,21 +10,10 @@ interface FeedNavigationProps {
 }
 
 const FeedNavigation = ({ feeds }: FeedNavigationProps) => {
-  const params = useParams<{ feedId: string }>();
-
   return (
     <>
       {feeds.map((feed) => (
-        <Link
-          key={feed.id}
-          href={`/feed/${feed.id}`}
-          className={cn(
-            feed.id === parseInt(params.feedId)
-              ? buttonVariants({ variant: "default", size: "sm" })
-              : buttonVariants({ variant: "ghost", size: "sm" }),
-            "max-w-52 justify-start font-bold",
-          )}
-        >
+        <LeftNavigationLink key={feed.id} href={`/feed/${feed.id}`}>
           <NewspaperIcon className="mr-2 h-4 w-4" />
           <div className="mr-2 truncate">{feed.title}</div>
           {feed._count.articles !== 0 && (
@@ -36,7 +21,7 @@ const FeedNavigation = ({ feeds }: FeedNavigationProps) => {
               {feed._count.articles}
             </Badge>
           )}
-        </Link>
+        </LeftNavigationLink>
       ))}
     </>
   );
