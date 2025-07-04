16654e28f85560ed837d37949a066b7104701f0d
Marcus Legendre <marcus.legendre@gmail.com>
2025-07-05 00:27:10 +0200

    test: add new authentication redirect test and refactor frontpage tests

--- tests/e2e/frontpage.spec.ts
+++ tests/e2e/frontpage.spec.ts
@@ -1,14 +1,18 @@
 import { expect, test } from "@playwright/test";
 
-test("has title", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("/");
+});
 
+test("has title", async ({ page }) => {
   await expect(page).toHaveTitle(/Briefing Officer/);
 });
 
 test("has logo that links to base url", async ({ page }) => {
-  await page.goto("/");
-
   const logo = page.getByRole("link", { name: "Briefing Officer" });
   await expect(logo).toHaveAttribute("href", "/");
+});
+
+test("redirects to sign-in page if not authenticated", async ({ page }) => {
+  await expect(page).toHaveURL("/sign-in");
 });
