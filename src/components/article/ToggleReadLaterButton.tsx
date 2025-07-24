b53f0d0b10c5023ede953068dd1d0ac0ad9eaef0
Marcus Legendre <marcus.legendre@gmail.com>
2025-07-24 22:29:31 +0200

    feat: add toast notifications for adding/removing articles to/from Read Later list

--- src/components/article/ToggleReadLaterButton.tsx
+++ src/components/article/ToggleReadLaterButton.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/lib/repository/articleRepository";
 import { Article } from "@prisma/client";
 import { BookmarkIcon } from "lucide-react";
+import { toast } from "sonner";
 
 interface ToggleReadLaterButtonProps {
   article: Article;
@@ -19,10 +20,36 @@ interface ToggleReadLaterButtonProps {
 const ToggleReadLaterButton = ({ article }: ToggleReadLaterButtonProps) => {
   const handleReadLaterClick = async () => {
     await markArticleAsReadLater(article.id);
+
+    toast("Article Added To Read Later", {
+      description: (
+        <>
+          <span className="font-semibold italic">{article.title}</span> has been
+          add to the Read Later list..
+        </>
+      ),
+      action: {
+        label: "Undo",
+        onClick: async () => await unmarkArticleAsReadLater(article.id),
+      },
+    });
   };
 
   const handleRemoveFromReadLaterClick = async () => {
     await unmarkArticleAsReadLater(article.id);
+
+    toast("Article Removed From Read Later", {
+      description: (
+        <>
+          <span className="font-semibold italic">{article.title}</span> has been
+          removed from the Read Later list..
+        </>
+      ),
+      action: {
+        label: "Undo",
+        onClick: async () => await markArticleAsReadLater(article.id),
+      },
+    });
   };
 
   if (article.readLater) {
