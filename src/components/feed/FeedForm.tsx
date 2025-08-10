f23bb7f00672aa6752be0c3e1a7eb3952d90f564
Marcus Legendre <marcus.legendre@gmail.com>
2025-08-10 10:43:33 +0200

    chore: remove text for uncategorized option in feed form

--- src/components/feed/FeedForm.tsx
+++ src/components/feed/FeedForm.tsx
@@ -125,7 +125,7 @@ const FeedForm = ({ editFeed, onSubmitComplete }: FeedFormProps) => {
                     }
                     className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                   >
-                    <option value="">Uncategorized</option>
+                    <option value=""></option>
                     {categories.map((category) => (
                       <option key={category.id} value={category.id}>
                         {category.name}
