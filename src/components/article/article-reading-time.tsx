1ba7aae4f5d2c3b61f2aa28859fbc4c32b2c842e
Marcus Legendre <marcus.legendre@gmail.com>
2025-08-22 16:12:05 +0200

    style: reformat `ArticleReadingTime` code

--- src/components/article/article-reading-time.tsx
+++ src/components/article/article-reading-time.tsx
@@ -5,7 +5,10 @@ export interface ArticleReadingTimeProps {
   words: number;
 }
 
-export const ArticleReadingTime = ({ text, words }: ArticleReadingTimeProps) => {
+export const ArticleReadingTime = ({
+  text,
+  words,
+}: ArticleReadingTimeProps) => {
   return (
     <div className="text-muted-foreground flex items-center gap-4 text-xs">
       <span className="flex items-center gap-1">
