6331e0539d7b119b23f45e0e6f5b58bd977ba2a0
Marcus Legendre <marcus.legendre@gmail.com>
2025-12-28 13:00:51 +0100

    feat!: model name is now configurable and must be set as environment variable
    LLM providers Anthropic and Azure OpenAI no longer have a hardcoded model name. Depending on the provider you're using, set either the `AZURE_OPENAI_MODEL` or `ANTHROPIC_MODEL` environment variable.

--- src/lib/ai/providers/azureOpenAi.ts
+++ src/lib/ai/providers/azureOpenAi.ts
@@ -5,12 +5,13 @@ import { createAzure } from "@ai-sdk/azure";
 
 const apiKey = process.env.AZURE_OPENAI_API_KEY;
 const resourceName = process.env.AZURE_OPENAI_RESOURCE_NAME;
+const model = process.env.AZURE_OPENAI_MODEL;
 
 const azureOpenAiModel: AiLanguageModelFactory = async () => ({
-  isConfigured: !!apiKey && !!resourceName,
+  isConfigured: !!apiKey && !!model && !!resourceName,
   create: async () => {
     const azureOpenAi = createAzure({ apiKey, resourceName });
-    return azureOpenAi("gpt-5-nano");
+    return azureOpenAi(model!);
   },
 });
 
