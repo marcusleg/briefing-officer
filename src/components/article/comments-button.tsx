76a8d136e597b4c448e718a4c6813e26eeb5896d
Marcus Legendre <marcus.legendre@gmail.com>
2026-06-04 14:47:11 +0200

    feat: add CommentsButton component

---
+++ src/components/article/comments-button.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { buttonVariants } from "@/components/ui/button";
+import { Article } from "@prisma/client";
+import { MessageSquare } from "lucide-react";
+import Link from "next/link";
+
+interface CommentsButtonProps {
+  article: Pick<Article, "commentsLink">;
+}
+
+const CommentsButton = ({ article }: CommentsButtonProps) => {
+  if (!article.commentsLink) return null;
+
+  return (
+    <Link
+      className={buttonVariants({
+        className: "text-xs",
+        size: "sm",
+        variant: "ghost",
+      })}
+      href={article.commentsLink}
+      target="_blank"
+      referrerPolicy="no-referrer"
+    >
+      <MessageSquare className="mr-1 size-4" />
+      Comments
+    </Link>
+  );
+};
+
+export default CommentsButton;
